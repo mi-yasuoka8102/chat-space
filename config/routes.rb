da742f20445eac0d0fc8e499dea50f4a2f9c811c
mi-yasuoka8102 <mizukiyasuoka23@gmail.com>
2018-09-14 14:24:09 +0900

    Add message model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root to: 'messages#index'
   root to: 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
