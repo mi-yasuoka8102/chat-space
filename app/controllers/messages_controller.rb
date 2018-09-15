c2ec147c81777f1daad492e492a845860c84e4d4
mi-yasuoka8102 <mizukiyasuoka23@gmail.com>
2018-09-15 15:26:23 +0900

    edit messages/index.haml

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    @memders = @group.users
   end
 
   def create
