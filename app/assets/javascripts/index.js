f2a800e977eec1379d8958f222b99210402dfa3e
mi-yasuoka8102 <mizukiyasuoka23@gmail.com>
2018-09-27 14:15:12 +0900

    Edit _form.html.haml
    フォームの送信時に自分自身のユーザーidが送れていない不具合の修正。

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,7 @@
 $(function() {
 
   var search_list = $("#user-search-result");
+  var group_user_list = $("#chat-group-users");
 
   function appendUsers(user) {
     var html = `<div class="chat-group-user clearfix">
@@ -10,9 +11,18 @@ $(function() {
     search_list.append(html);
   }
 
-  $("#user-search-field").on('keyup', function() {
+
+  function addUserToGroup(user_name,user_id) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+                  <p class='chat-group-user__name'>${ user_name }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    group_user_list.append(html);
+  }
+
+  $("#user-search-field").on('keyup', function() {//インクリメントサーチ機能
     var input = $("#user-search-field").val();
-    console.log(input);
     $.ajax({
     type: 'GET',
     url: '/users',
@@ -30,5 +40,16 @@ $(function() {
     .fail(function() {
       alert('ユーザー検索に失敗しました');
     });
+  });
+
+  $("#user-search-result").on('click','.chat-group-user__btn--add', function() {//グループにユーザーを追加する
+    $(this).parent().remove();
+    var user_name = $(this).attr('data-user-name');
+    var user_id = $(this).attr('data-user-id');
+    addUserToGroup(user_name,user_id);
+  });
+
+  $("#chat-group-users").on('click','.chat-group-user__btn--remove', function() {//グループからユーザーを削除する
+    $(this).parent().remove();
   });
 });
